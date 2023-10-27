12b4fe67be6f4bb40a875bc1196c9e28c96982d8
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-10-27 12:29:24 +0200

    project medium

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const change = 420;
+const modBtn = document.getElementById("btnG");
+const headCh = document.getElementById("head");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= change) {
+    modBtn.classList.add("btnG");
+    headCh.classList.add("headCh");
+    modBtn.classList.remove("outBtn");
+    headCh.classList.remove("outHead");
+  } else if (window.screenY < change) {
+    modBtn.classList.remove("btnG");
+    headCh.classList.remove("headCh");
+    modBtn.classList.add("outBtn");
+    headCh.classList.add("outHead");
+  }
+});
+
+let m = document.querySelectorAll("g[stroke-linecap='butt']");
+setInterval(() => {
+  let random = Math.round(Math.random() * (m.length - 3));
+  m[random].classList.toggle("opacity");
+}, 10);
